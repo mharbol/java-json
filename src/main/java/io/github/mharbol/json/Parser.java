b9253f1f6aa937ce3b3d5ce00e5e02b60c15c71d
Marcus Harbol <mharbol@users.noreply.github.com>
2024-07-13 22:12:10 -0600

    parser sans parseNumber

--- src/main/java/io/github/mharbol/json/Parser.java
+++ src/main/java/io/github/mharbol/json/Parser.java
@@ -11,13 +11,86 @@ public class Parser {
 
     private final List<Token> tokens;
     private Iterator<Token> iter;
+    private Token currToken;
 
     public Parser(List<Token> tokens) {
         this.tokens = tokens;
         this.iter = this.tokens.iterator();
     }
 
-    public JSONObject parseObject() {
+    public JSONObject parse() {
+        iter.next();
+        return parseObject();
+    }
+
+    private JSONObject parseObject() {
+        JSONObject object = new JSONObject();
+        currToken = iter.next(); // string key or close brace
+        while (TokenTypeEnum.CLOSE_BRACE != currToken.tokenType) {
+            String key = currToken.value;
+            iter.next(); // colon
+            iter.next(); // value
+            object.put(key, parseValue());
+            iter.next(); // comma or close brace
+        }
+        return object;
+    }
+
+    private JSONValue parseValue() {
+        switch (currToken.tokenType) {
+            case TRUE:
+                return new JSONBoolean(true);
+            case FALSE:
+                return new JSONBoolean(false);
+            case NULL:
+                return JSONNull.NULL;
+            case STRING:
+                return new JSONString(currToken.value);
+            case OPEN_BRACE:
+                return parseObject();
+            case OPEN_BRACKET:
+                return parseArray();
+            case NUMBER:
+                return parseNumber();
+            default:
+                return null; // never should get here since the Tokesn are verified
+        }
+    }
+
+    private JSONArray parseArray() {
+        JSONArray array = new JSONArray();
+        currToken = iter.next(); // value or close bracket
+        while (TokenTypeEnum.CLOSE_BRACKET != currToken.tokenType) {
+            array.add(parseValue());
+            iter.next(); // comma or close bracket
+        }
+        return array;
+    }
+
+    private JSONNumber parseNumber() {
+        // see if the number decimal, float, or scientific
+        final int sciIdx = currToken.value.toLowerCase().indexOf('e');
+        final int decIdx = currToken.value.indexOf('.');
+        if (-1 != sciIdx) {
+            return parseSci();
+        } else if (-1 != decIdx) {
+            return parseDouble();
+        } else {
+            return parseInt();
+        }
+    }
+
+    private JSONNumber parseInt() {
+        // TODO
+        return null;
+    }
+
+    private JSONNumber parseDouble() {
+        // TODO
+        return null;
+    }
+
+    private JSONNumber parseSci() {
         // TODO
         return null;
     }
